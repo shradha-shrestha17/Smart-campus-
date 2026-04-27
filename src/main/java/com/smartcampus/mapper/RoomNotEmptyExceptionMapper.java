9f986c46556b39ab2e11802ebd9465b5346b07aa
shradha-shrestha17 <shresthashradha17@gmail.com>
2026-04-27 15:16:34 +0545

    add RoomNotEmptyExceptionMapper to handle RoomNotEmptyException responses

--- src/main/java/com/smartcampus/mapper/RoomNotEmptyExceptionMapper.java
+++ src/main/java/com/smartcampus/mapper/RoomNotEmptyExceptionMapper.java
@@ -1,5 +1,19 @@
 package com.smartcampus.mapper;
 
-public class RoomNotEmptyExceptionMapper {
-    
+import com.smartcampus.exception.RoomNotEmptyException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.Map;
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    @Override
+    public Response toResponse(RoomNotEmptyException e) {
+        return Response.status(Response.Status.CONFLICT)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(Map.of("status", 409, "error", "Room Conflict", "message", e.getMessage()))
+                .build();
+    }
 }
